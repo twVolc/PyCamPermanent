7e652c539608ad83c322c815d8372d5acc805dad
twvolc <tcwilkes1@sheffield.ac.uk>
2024-02-22 08:48:27 +0000

    Tidying video_writer
    Just small tidy of video writer

--- pycam/scripts/video_writer.py
+++ pycam/scripts/video_writer.py
@@ -9,7 +9,7 @@ from pycam.io_py import create_video
 import os
 
 # Define directory where data directories are located
-parent_directory = 'X:/volcano_cameras/Shared/Cotopaxi/'
+parent_directory = 'X:/volcano_cameras/Shared/Cotopaxi/2024/'
 
 # Define band to write
 band = 'on'
@@ -17,7 +17,6 @@ band = 'on'
 data_dirs = os.listdir(parent_directory)
 
 for data_dir in data_dirs:
-
     dir_path = os.path.join(parent_directory, data_dir)
     if os.path.isdir(dir_path):
         create_video(dir_path, band=band, save_dir=dir_path, overwrite=False)
