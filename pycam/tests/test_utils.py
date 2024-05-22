1419564666687e9f5a14cfda4e4751ab52e1e3d1
Dan Brady <daniel.brady@sheffield.ac.uk>
2024-05-22 22:06:37 +0100

    more testing conditons

--- pycam/tests/test_utils.py
+++ pycam/tests/test_utils.py
@@ -2,8 +2,11 @@ import pytest
 from pycam.utils import truncate_path
 
 normal_test_data = [
+    ("tiny path", 10, "tiny path"),
+    ("this is a short path", 10, "...short path"),
     ("this is a short path", 25, "this is a short path"),
     ("this is a much, much longer path", 25, "... a much, much longer path"),
+    ("this is a much, much longer path", 40, "this is a much, much longer path"),
 ]
 
 @pytest.mark.parametrize("path, max_length, expected", normal_test_data)
@@ -14,7 +17,7 @@ def test_truncate_path_normal(path, max_length, expected):
 error_test_data = [
     ("", 20, "path length should be greater than 0"),
     ("normal string", 0, "max_length should be greater than 0"),
-    ("another normal string", -1, "max_length should be greater than 0")
+    ("another normal string", -1, "max_length should be greater than 0"),
 ]
 
 @pytest.mark.parametrize("path, max_length, msg", error_test_data)
