a6d3f6aca5a3478142ec0506e859e6b42b4ccd21
Dan Brady <daniel.brady@sheffield.ac.uk>
2024-06-06 16:49:24 +0100

    move common init features to SpecWorker

--- pycam/doas/ifit_worker.py
+++ pycam/doas/ifit_worker.py
@@ -36,6 +36,7 @@ from ifit.spectral_analysis import Analyser
 from ifit.light_dilution import generate_ld_curves
 from pycam.ifit_ld import lookup
 from pydoas.analysis import DoasResults
+from pycam.doas.spec_worker import SpecWorker
 
 import shapely
 
@@ -45,7 +46,7 @@ except BaseException:
     N_A = 6.022140857e+23
 
 
-class IFitWorker:
+class IFitWorker(SpecWorker):
     """
     Class to control DOAS processing
     General order of play for processing:
@@ -58,9 +59,7 @@ class IFitWorker:
     """
     def __init__(self, routine=2, species={'SO2': {'path': '', 'value': 0}}, spec_specs=SpecSpecs(), spec_dir='C:\\',
                  dark_dir=None, q_doas=queue.Queue(), frs_path='./pycam/doas/calibration/sao2010.txt'):
-        self.routine = routine          # Defines routine to be used, either (1) Polynomial or (2) Digital Filtering
-
-        self.spec_specs = spec_specs    # Spectrometer specifications
+        super().__init__(routine, species, spec_specs, spec_dir, dark_dir, q_doas)
 
         # ======================================================================================================================
         # Initial Definitions
@@ -68,120 +67,34 @@ class IFitWorker:
         self.time_zone = 0              # Time zone for adjusting data times on load-in (relative to UTC)
 
         self.ppmm_conversion = 2.652e15   # convert absorption cross-section in cm2/molecule to ppm.m (MAY NEED TO CHANGE THIS TO A DICTIONARY AS THE CONVERSION MAY DIFFER FOR EACH SPECIES?)
-        self._conversion_factor = 2.663 * 1e-6  # Conversion for ppm.m into Kg m-2
-        MOL_MASS_SO2 = 64.0638  # g/mol
-        self.ppmm_conv = (self._conversion_factor * N_A * 1000) / (
-                100 ** 2 * MOL_MASS_SO2)  # Conversion for ppm.m to molecules cm-2
 
-        self.shift = 0                  # Shift of spectrum in number of pixels
-        self.shift_tol = 0              # Shift tolerance (will process data at multiple shifts defined by tolerance)
-        self.stretch = 0                # Stretch of spectrum
-        self.stretch_tol = 0            # As shift_tol but for stretch
-        self.stretch_adjuster = 0.0001  # Factor to scale stretch (needed if different spectrometers have different pixel resolutions otherwise the stretch applied may be in too large or too small stages)
-        self.stretch_resample = 100     # Number of points to resample the spectrum by during stretching
-        self._start_stray_pix = None    # Pixel space stray light window definitions
-        self._end_stray_pix = None
         self._start_stray_wave = 293    # Wavelength space stray light window definitions
         self._end_stray_wave = 296
-        self._start_fit_pix = None  # Pixel space fitting window definitions
-        self._end_fit_pix = None
         self._start_fit_wave_init = 300  # Wavelength space fitting window definitions       Set big range to start Analyser
         self._end_fit_wave_init = 340
         self._start_fit_wave = 308       # Update fit window to more reasonable starting size (initial setting was to create a big grid
         self._end_fit_wave = 318
         self.start_fit_wave_2 = 312      # Second fit window (used in light dilution correction)
         self.end_fit_wave_2 = 322
-        self.fit_window = None      # Fitting window, determined by set_fit_window()
-        self.fit_window_ref = None  # Placeholder for shifted fitting window for the reference spectrum
-        self.wave_fit = True        # If True, wavelength parameters are used to define fitting window
 
-        self.wavelengths = None         # Placeholder for wavelengths attribute which contains all wavelengths of spectra
-        self.wavelengths_cut = None     # Wavelengths in fit window
-        self._dark_spec = None           # Dark spectrum
-        self.dark_dict = {}             # Dictionary holding all dark spectra loaded in
-        self._clear_spec_raw = None     # Clear (fraunhofer) spectrum - not dark corrected
-        self._plume_spec_raw = None     # In-plume spectrum (main one which is used for calculation of SO2
-        self.clear_spec_corr = None     # Clear (fraunhofer) spectrum - typically dark corrected and stray light corrected
-        self.plume_spec_corr = None     # In-plume spectrum (main one which is used for calculation of SO2
         self.spec_time = None           # Time of currently loaded plume_spec
-        self.ref_spec = dict()          # Create empty dictionary for holding reference spectra
-        self.ref_spec_interp = dict()   # Empty dictionary to hold reference spectra after sampling to spectrometer wavelengths
-        self.ref_spec_conv = dict()     # Empty dictionary to hold reference spectra after convolving with ILS
-        self.ref_spec_cut = dict()      # Ref spectrum cut to fit window
-        self.ref_spec_ppmm = dict()   # Convolved ref spectrum scaled by ppmm_conversion factor
-        self.ref_spec_filter = dict()   # Filtered reference spectrum
-        self.ref_spec_fit = dict()      # Ref spectrum scaled by ppmm (for plotting)
-        self.ref_spec_types = ['SO2', 'O3', 'Ring'] # List of reference spectra types accepted/expected
         self.ref_spec_used = list(species.keys())   # Reference spectra we actually want to use at this time (similar to ref_spec_types - perhaps one is obsolete (or should be!)
-        self.abs_spec = None
-        self.abs_spec_cut = None
-        self.abs_spec_filt = None
-        self.abs_spec_species = dict()  # Dictionary of absorbances isolated for individual species
-        self.ILS_wavelengths = None     # Wavelengths for ILS
-        self._ILS = None                 # Instrument line shape (will be a numpy array)
         self.ils_path = None
-        self.processed_data = False     # Bool to define if object has processed DOAS yet - will become true once process_doas() is run
-
+        
         self.poly_order = 2  # Order of polynomial used to fit residual
 
-        self.start_ca = -2000  # Starting column amount for iterations
-        self.end_ca = 20000  # Ending column amount for iterations
-        self.vals_ca = np.arange(self.start_ca, self.end_ca+1)  # Array of column amounts to be iterated over
-        self.vals_ca_cut_idxs = np.arange(0, len(self.vals_ca), 100)
-        self.vals_ca_cut = self.vals_ca[self.vals_ca_cut_idxs]
-        self.mse_vals_cut = np.zeros(len(self.vals_ca_cut))
-        self.mse_vals = np.zeros(len(self.vals_ca))  # Array to hold mse values
-
-        self.std_err = None
         self.fit_errs = dict()
-        self.column_density = dict()
-
-        self.filetypes = dict(defaultextension='.png', filetypes=[('PNG', '*.png')])
-
-        # ----------------------------------------------------------------------------------
-        # We need to make sure that all images are dark subtracted before final processing
-        # Also make sure that we don't dark subtract more than once!
-        self.ref_convolved = False  # Bool defining if reference spe has been convolved - speeds up DOAS processing
-        self.new_spectra = True
-        self.dark_corrected_clear = False
-        self.dark_corrected_plume = False
-        self.stray_corrected_clear = False    # Bool defining if all necessary spectra have been stray light corrected
-        self.stray_corrected_plume = False    # Bool defining if all necessary spectra have been stray light corrected
-
-        self.have_dark = False  # Used to define if a dark image is loaded.
-        self.cal_dark_corr = False  # Tells us if the calibration image has been dark subtracted
-        self.clear_dark_corr = False  # Tells us if the clear image has been dark subtracted
-        self.plume_dark_corr = False  # Tells us if the plume image has been dark subtracted
-        # ==============================================================================================================
 
         # Processing loop attributes
-        self.process_thread = None      # Thread for running processing loop
-        self.processing_in_thread = False   # Flags whether the object is processing in a thread or in the main thread - therefore deciding whether plots should be updated herein or through pyplisworker
-        self.q_spec = queue.Queue()     # Queue where spectra files are placed, for processing herein
-        self.q_doas = q_doas
         self.q_stop = queue.Queue()     # Queue for stopping processing of sequence
-        self.watcher = None
-        self.watching_dir = None
-        self.watching = False
         self.STOP_FLAG = 'exit'
         self.plot_iter = True           # Plots time series plot iteratively if true
 
-        self._dark_dir = None
-        self.dark_dir = dark_dir        # Directory where dark images are stored
-        self.spec_dir = spec_dir        # Directory where plume spectra are stored
-        self.spec_dict = {}             # Dictionary containing all spectrum files from current spec_dir
-
         # Figures
-        self.fig_spec = None            # pycam.doas.SpectraPlot object
-        self.fig_doas = None            # pycam.doas.DOASPlot object
         self.fig_series = None          # pycam.doas.CDSeries object
-        self.dir_info = None            
 
         # Results object
-        self.results = DoasResults([], index=[], fit_errs=[], species_id='SO2')
         self.results.ldfs = []
-        self.save_date_fmt = '%Y-%m-%dT%H%M%S'
-        self.save_freq = [0]
 
         # ==============================================================================================================
         # iFit setup
