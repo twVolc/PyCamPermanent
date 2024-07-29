8e42dbb82ac8d17868d38ad4df1e385e833fd9b5
Dan Brady <daniel.brady@sheffield.ac.uk>
2024-07-29 12:05:17 +0100

    update get_img to rely on error handling in img_loader

--- pycam/so2_camera_processor.py
+++ pycam/so2_camera_processor.py
@@ -39,7 +39,6 @@ import os
 import cv2
 from skimage import transform as tf
 import warnings
-import traceback
 from ruamel.yaml import YAML
 from inspect import cleandoc
 warnings.simplefilter("ignore", UserWarning)
@@ -248,7 +247,6 @@ class PyplisWorker:
         self.img_tau_prev = copy.deepcopy(self.img_A)
         self.img_cal = None         # Calibrated image
         self.img_cal_prev = None
-        self.test_img = copy.deepcopy(self.img_A)
 
         # Calibration attributes
         self.got_doas_fov = False
@@ -1188,23 +1186,21 @@ class PyplisWorker:
 
         self.prep_img(img, img_path, band, plot, temporary)
 
-
     def get_img(self, img_path, attempts = 1):
         
         while attempts > 0:
             # Try and load the image
-            img = pyplis.image.Img(img_path, self.load_img_func)
-
-            # If successful then leave the loop
-            if img.img is not None:
+            try:
+                img = pyplis.image.Img(img_path, self.load_img_func)
+            except FileNotFoundError as e:
+                err = e
+                time.sleep(0.2)
+                attempts -= 1
+            else:
                 break
-            
-            # Otherwise wait half a second and decrease the number of attempts left
-            time.sleep(0.1)
-            attempts -= 1
         else:
             # This will run once the number of repeats is 0
-            raise FileNotFoundError(f"Image from {img_path} could not be loaded. Skipping pair.")
+            raise err
 
         img.filename = img_path.split('\\')[-1].split('/')[-1]
         img.pathname = img_path
@@ -3577,12 +3573,6 @@ class PyplisWorker:
         if img_path_B is not None:
             img_B = self.get_img(img_path_B, attempts=3)
 
-        try:
-            img = img_A - self.test_img
-            img = img_B - self.test_img
-        except (TypeError, AttributeError) as e:
-            raise FileNotFoundError('{}'.format(e))
-
         # Can pass None to this function for img paths, and then the current images will be processed
         if img_path_A is not None:
             self.prep_img(img_A, img_path_A, band='A', plot=plot)
@@ -3984,8 +3974,9 @@ class PyplisWorker:
             # Process the pair
             try:
                 self.process_pair(img_path_A, img_path_B, plot=self.plot_iter)
-            except FileNotFoundError:
-                traceback.print_exc()
+            except FileNotFoundError as e:
+                print(e)
+                print("Skipping pair")
                 continue
 
             # Save all images that have been requested
